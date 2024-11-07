0665fd945b6664fe91ea7d3634c16121c66c631e
Artezy <artezyishere@gmail.com>
2024-11-07 18:24:42 +0700

    Create main.py

---
+++ Telegram/HamsterKombatS2/main.py
@@ -0,0 +1,15 @@
+import sys
+
+from src.launcher import main
+from src.deeplchain import banner, log, mrh
+
+if __name__ == "__main__":
+    while True:
+        try:
+            banner()
+            main()
+        except KeyboardInterrupt:
+            print()
+            log(mrh + f"Successfully logged out of the bot\n")
+            sys.exit()
+          
