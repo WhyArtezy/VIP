0c885cd34ea2d2045ea9b04936aeb4597719d69c
Artezy <artezyishere@gmail.com>
2024-11-26 22:53:28 +0700

    Create headers.py

---
+++ Telegram/Vooi/src/headers.py
@@ -0,0 +1,17 @@
+from src.agent import generate_random_user_agent
+
+def headers():
+    return {
+            "Accept": "application/json, text/plain, */*",
+            "Accept-Language": "vi-VN,vi;q=0.9,fr-FR;q=0.8,fr;q=0.7,en-US;q=0.6,en;q=0.5",
+            "Content-Type": "application/json",
+            "Origin": "https://app.tg.vooi.io",
+            "Referer": "https://app.tg.vooi.io/",
+            "Sec-Ch-Ua": '"Not/A)Brand";v="99", "Google Chrome";v="115", "Chromium";v="115"',
+            "Sec-Ch-Ua-Mobile": "?0",
+            "Sec-Ch-Ua-Platform": '"Windows"',
+            "Sec-Fetch-Dest": "empty",
+            "Sec-Fetch-Mode": "cors",
+            "Sec-Fetch-Site": "same-site",
+            'User-Agent': generate_random_user_agent()
+    }
